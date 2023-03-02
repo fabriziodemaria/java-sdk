7e2cff47b88782ba4e455ac4d562a9e6c9a510f7
Fabrizio Demaria <fdema@spotify.com>
2023-03-02 11:19:07 +0100

    Test different hook types with single hookcontext type

--- src/test/java/dev/openfeature/sdk/HookSupportTest.java
+++ src/test/java/dev/openfeature/sdk/HookSupportTest.java
@@ -3,7 +3,6 @@ package dev.openfeature.sdk;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,17 +26,40 @@ class HookSupportTest implements HookFixtures {
         attributes.put("baseKey", new Value("baseValue"));
         EvaluationContext baseContext = new ImmutableContext(attributes);
         HookContext<String> hookContext = new HookContext<>("flagKey", FlagValueType.STRING, "defaultValue", baseContext, () -> "client", () -> "provider");
-        Hook<String> hook1 = mockStringHook();
-        Hook<String> hook2 = mockStringHook();
-        when(hook1.before(any(), any())).thenReturn(Optional.of(evaluationContextWithValue("bla", "blubber")));
-        when(hook2.before(any(), any())).thenReturn(Optional.of(evaluationContextWithValue("foo", "bar")));
+        Hook<String> stringHook = new TestStringHook();
+        Hook<Boolean> boolHook = new TestBoolHook();
         HookSupport hookSupport = new HookSupport();
 
-        EvaluationContext result = hookSupport.beforeHooks(FlagValueType.STRING, hookContext, Arrays.asList(hook1, hook2), Collections.emptyMap());
+        EvaluationContext result = hookSupport.beforeHooks(FlagValueType.STRING, hookContext, Arrays.asList(stringHook, boolHook), Collections.emptyMap());
 
-        assertThat(result.getValue("bla").asString()).isEqualTo("blubber");
-        assertThat(result.getValue("foo").asString()).isEqualTo("bar");
+        assertThat(result.getValue("bla").asString()).isEqualTo("defaultValueTest");
+//        assertTrue(result.getValue("foo").asBoolean()); ???
         assertThat(result.getValue("baseKey").asString()).isEqualTo("baseValue");
+    }
+
+    static class TestStringHook implements Hook<String> {
+        @Override
+        public Optional<EvaluationContext> before(HookContext<String> ctx,
+            Map<String, Object> hints) {
+            String dv = ctx.getDefaultValue();
+            Map<String, Value> attributes = new HashMap<>();
+            attributes.put("bla", new Value(dv + "Test"));
+            EvaluationContext baseContext = new ImmutableContext(attributes);
+            return Optional.of(baseContext);
+        }
+    }
+
+    static class TestBoolHook implements Hook<Boolean> {
+        @Override
+        public Optional<EvaluationContext> before(HookContext<Boolean> ctx,
+            Map<String, Object> hints) {
+            Boolean dv = ctx.getDefaultValue();
+            Map<String, Value> attributes = new HashMap<>();
+            attributes.put("foo", new Value(dv));
+            EvaluationContext baseContext = new ImmutableContext(attributes);
+            return Optional.of(baseContext);
+        }
+
     }
 
     @ParameterizedTest
